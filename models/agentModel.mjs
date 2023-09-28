5bdf0ee76050bf9419f52f3d999132e1b4630399
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 22:07:05 -0700

    Update agentModel.mjs

--- models/agentModel.mjs
+++ models/agentModel.mjs
@@ -1,20 +1,32 @@
-// agentModel.mjs
+import sequelize from '../config/database.mjs';
+import { DataTypes } from 'sequelize';
+import Booking from './bookingModel.mjs';  // Assuming you have this model defined
+import User from './userModel.mjs';  // Assuming you have this model defined
 
-import { db } from '../db/connection.mjs';
+const Agent = sequelize.define('Agent', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    // There might be other attributes like 'role' based on the previous structure
+});
 
-// Fetch all agents from the database
-export const fetchAllAgents = async () => {
-    try {
-        const query = `
-            SELECT id, name, email 
-            FROM agents
-        `;
+// Define the relationships
+Agent.hasMany(Booking, {
+    foreignKey: 'agentId', // Assuming the 'Booking' table has 'agentId' as the foreign key
+    as: 'bookings',  // Alias if you fetch an agent's bookings: agent.getBookings()
+    onDelete: 'CASCADE'  // If an agent is deleted, its associated bookings will be deleted too.
+});
 
-        const [rows] = await db.execute(query);
-        return rows;
+Agent.hasMany(User, {
+    foreignKey: 'agentId', // Assuming the 'User' table has 'agentId' as the foreign key
+    as: 'users',  // Alias if you fetch an agent's users: agent.getUsers()
+    onDelete: 'CASCADE'  // If an agent is deleted, its associated users will be deleted too.
+});
 
-    } catch (error) {
-        console.error(`Error fetching agents: ${error.message}`);
-        throw error;
-    }
-};
+export default Agent;
