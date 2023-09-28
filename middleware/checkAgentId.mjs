60fb3f959e5b2026cf5cde536349eb91ba4ca6a9
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 22:13:29 -0700

    Update checkAgentId.mjs

--- middleware/checkAgentId.mjs
+++ middleware/checkAgentId.mjs
@@ -1,34 +1,28 @@
-// Middleware to validate and check the "X-Agent-Id" header
+import Agent from '../models/agentModel.mjs';
 
-/**
- * This middleware checks if the "X-Agent-Id" header is present in the incoming request.
- * If the header is present, it attaches the agentId to the request object for future use.
- * Otherwise, it sends a 400 Bad Request response indicating the missing header.
- *
- * @param {object} req - Express request object
- * @param {object} res - Express response object
- * @param {function} next - Express next middleware function
- */
-function checkAgentId(req, res, next) {
-    const agentIdHeader = req.headers['x-agent-id'];
+const checkAgentId = async (req, res, next) => {
+    const agentId = req.headers['x-agent-id'];
 
-    // Check if the X-Agent-Id header is missing
-    if (!agentIdHeader) {
-        return res.status(400).json({
-            status: 'failure',
-            message: 'X-Agent-Id header is missing.'
-        });
+    if (!agentId) {
+        return res.status(400).send({ error: 'X-Agent-Id header is missing.' });
     }
 
-    // Further validations can be added here. For example:
-    // - Check if the agentId format is correct
-    // - Check if the agentId exists in the system/database (though this might be overkill for just middleware)
+    try {
+        // Check if the agent with the provided ID exists in the database
+        const agent = await Agent.findByPk(agentId);
 
-    // Attach the agentId to the request object
-    req.agentId = agentIdHeader;
+        if (!agent) {
+            return res.status(404).send({ error: 'Agent not found.' });
+        }
 
-    // Continue to the next middleware/route handler
-    next();
-}
+        // Store the agent information in the request for further processing in subsequent routes/middleware
+        req.agent = agent;
+
+        next();
+    } catch (error) {
+        console.error('Error verifying Agent ID:', error);
+        res.status(500).send({ error: 'Internal Server Error.' });
+    }
+};
 
 export default checkAgentId;
