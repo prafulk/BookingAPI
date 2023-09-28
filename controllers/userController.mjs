9c6fca84d3ed29d69a27ff942e07e62b2a780c45
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 22:51:41 -0700

    Update userController.mjs

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -1,38 +1,86 @@
-import { fetchUsersByAgent } from '../models/userModel.mjs';
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} from '../models/userModel.mjs';
 
-// Fetch all users for a specific agent
-export const getUsersByAgent = async (req, res) => {
+// Fetch all users that the specific agent can access
+const listAllUsersByAgent = async (req, res, next) => {
     try {
-        const agentId = req.headers['X-Agent-Id'];
-
-        // If no agent ID provided, return error
-        if (!agentId) {
-            return res.status(400).json({
-                success: false,
-                message: 'Missing required agent ID in headers.',
-            });
-        }
-
-        const users = await fetchUsersByAgent(agentId);
-
-        // If no users found for this agent
-        if (!users.length) {
-            return res.status(404).json({
-                success: false,
-                message: `No users found for agent ID: ${agentId}`,
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            data: users,
-        });
-
+        const agentId = req.agentId;
+        const users = await getAllUsers({ agentId });
+        res.status(200).json(users);
     } catch (error) {
-        console.error(`Error fetching users by agent: ${error.message}`);
-        res.status(500).json({
-            success: false,
-            message: 'Internal server error. Please try again later.',
-        });
+        next(error);
     }
+};
+
+// Fetch specific user details, including their bookings
+const getUserDetails = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const user = await getUserById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Create a new user
+const addUser = async (req, res, next) => {
+    try {
+        const userData = req.body;
+        const newUser = await createUser(userData);
+        res.status(201).json(newUser);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Update an existing user's details
+const modifyUser = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const userData = req.body;
+        const updatedUser = await updateUser(userId, userData);
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Delete a specific user
+const removeUser = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const result = await deleteUser(userId);
+
+        if (!result) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export {
+    listAllUsersByAgent,
+    getUserDetails,
+    addUser,
+    modifyUser,
+    removeUser
 };
