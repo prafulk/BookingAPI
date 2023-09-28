49542fda5eede25a2526282ff2be655f4ada98be
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 23:12:27 -0700

    Update schedulerRoutes.mjs

--- routes/schedulerRoutes.mjs
+++ routes/schedulerRoutes.mjs
@@ -1,9 +1,17 @@
 import express from 'express';
-import { getBookingsForWeek } from '../controllers/schedulerController.mjs';
+import {
+    getWeekBookings
+} from '../controllers/schedulerController.mjs';
+import { authorization } from '../middlewares/index.mjs';
 
 const router = express.Router();
 
-// Get bookings for a specific week
-router.get('/scheduler', getBookingsForWeek);
+// Get all bookings for a specific week (Client API)
+router.get('/scheduler', authorization('ADMIN', 'REGULAR'), getWeekBookings);
+
+// A scaffold for the Business API side of the scheduler
+router.get('/scheduler-business', (req, res) => {
+    res.status(200).send('OK');
+});
 
 export default router;
