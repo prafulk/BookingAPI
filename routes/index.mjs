fcdacdce98c630cb9175883fa672e99705d6417a
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 23:13:53 -0700

    Update index.mjs

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,15 +1,15 @@
 import express from 'express';
-import schedulerRoutes from './schedulerRoutes.mjs';
-import bookingRoutes from './bookingRoutes.mjs';
-import userRoutes from './userRoutes.mjs';
 import agentRoutes from './agentRoutes.mjs';
+import bookingRoutes from './bookingRoutes.mjs';
+import schedulerRoutes from './schedulerRoutes.mjs';
+import userRoutes from './userRoutes.mjs';
 
 const router = express.Router();
 
-// Combine all the routes
-router.use(schedulerRoutes);
-router.use(bookingRoutes);
-router.use(userRoutes);
-router.use(agentRoutes);
+// Mount the routes to their respective endpoints
+router.use('/agents', agentRoutes);
+router.use('/bookings', bookingRoutes);
+router.use('/scheduler', schedulerRoutes);
+router.use('/users', userRoutes);
 
 export default router;
