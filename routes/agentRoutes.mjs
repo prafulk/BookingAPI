07aecd2deaa010229db5604b9448b77839f8db5e
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 23:11:00 -0700

    Update agentRoutes.mjs

--- routes/agentRoutes.mjs
+++ routes/agentRoutes.mjs
@@ -1,9 +1,28 @@
 import express from 'express';
-import { getAllAgents } from '../controllers/agentController.mjs';
+import {
+    getAllAgents,
+    getAgentById,
+    createAgent,
+    updateAgent,
+    deleteAgent
+} from '../controllers/agentController.mjs';
+import { checkAgentId, authorization } from '../middlewares/index.mjs';
 
 const router = express.Router();
 
-// Get all agents
-router.get('/agents', getAllAgents);
+// Get all agents (ADMIN only)
+router.get('/agents', authorization('ADMIN'), getAllAgents);
+
+// Get a specific agent by ID (self-check and ADMIN)
+router.get('/agents/:id', checkAgentId, authorization('ADMIN', 'SELF'), getAgentById);
+
+// Create a new agent (ADMIN only, or possibly even restricted further)
+router.post('/agents', authorization('ADMIN'), createAgent);
+
+// Update an agent's details (self-check and ADMIN)
+router.put('/agents/:id', checkAgentId, authorization('ADMIN', 'SELF'), updateAgent);
+
+// Delete an agent by ID (ADMIN only, for security reasons)
+router.delete('/agents/:id', checkAgentId, authorization('ADMIN'), deleteAgent);
 
 export default router;
