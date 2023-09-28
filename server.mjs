618b8101ab2df790603caaf795ab0854683d5f05
P <4817076+prafulk@users.noreply.github.com>
2023-09-27 21:52:19 -0700

    Update server.mjs
    Import Combined Routes: At the top, we added an import statement for combinedRoutes from the index.mjs file in the routes directory.
    
    Use Combined Routes: After setting up the middlewares, we attach the combined routes to the Express app with app.use('/api', combinedRoutes);. This means that all the routes are now prefixed with /api.
    
    Removed Placeholder: The placeholder for other routes (// Placeholder for other routes like /scheduler, /booking, /users, /agents, etc.) is now effectively replaced by the combinedRoutes.
    
    Retained Basic Structure: The structure for initializing the Express app, setting up the middlewares, and starting the server remains unchanged.

--- server.mjs
+++ server.mjs
@@ -1,14 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// You can import other required modules/libraries as you progress
+import combinedRoutes from './routes/index.mjs';  // Import combined routes
 
 const app = express();
 const PORT = 3000;
 
 // Middlewares
+
+// Use body-parser to parse JSON requests
 app.use(bodyParser.json());
 
-// A middleware to check for the "X-Agent-Id" header
+// Middleware to check for the "X-Agent-Id" header
 app.use((req, res, next) => {
     if (!req.headers['x-agent-id']) {
         return res.status(400).send({ error: 'X-Agent-Id header is missing.' });
@@ -18,12 +20,13 @@ app.use((req, res, next) => {
     next();
 });
 
-// Sample routes (You can separate these into different route files later)
+// Use combined routes from the routes directory
+app.use('/api', combinedRoutes);
+
+// Sample route, which can be moved or replaced as your app evolves
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-
-// Placeholder for other routes like /scheduler, /booking, /users, /agents, etc.
 
 // Basic error handler
 app.use((err, req, res, next) => {
@@ -31,7 +34,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 });
-
